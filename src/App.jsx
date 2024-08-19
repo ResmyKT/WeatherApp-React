c0c3e9d265e79372af6f1c5b150ead68c73f3e6e
unknown <resmykt333@gmail.com>
2024-08-19 12:48:37 +0530

    Weather-App

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+import './App.css'
+import WeatherApp from './Components/WeatherApp/WeatherApp'
+
+function App() {
+
+
+  return (
+    <>
+     <div className="App">
+
+<WeatherApp/>
+
+<h3>Made By ResmyKT</h3>
+  
+</div>
+    </>
+  )
+}
+
+export default App
